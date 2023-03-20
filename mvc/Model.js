02effde1bb81672fad76d724c7eb646a4613441e
web developer <67851426+web-np@users.noreply.github.com>
2023-03-20 20:10:00 +0530

    Create Model.js

---
+++ mvc/Model.js
@@ -0,0 +1,12 @@
+const Model = {
+  data: [],
+  getData() {
+    // Code to fetch data from server
+  },
+  saveData() {
+    // Code to save data to server
+  },
+  deleteData() {
+    // Code to delete data from server
+  }
+}
