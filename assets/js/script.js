94367181b07cdaf10a3658ea5c57e2060e21889a
web developer <67851426+web-np@users.noreply.github.com>
2023-03-22 22:35:59 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 // JavaScript source code
 // progress bar auto count with %
 //countTab(r,i) { r = (i + 1) / r * 100;--r= elements; i=posizion of itm in loop
+//location.href.split('/').reverse()[0];
+//insertAdjacentHTML("beforeend","data");----append data
