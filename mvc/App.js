bc15633c4a98aefc33d3e8f566a2a590e6eceb79
web developer <67851426+web-np@users.noreply.github.com>
2023-03-27 23:32:18 +0530

    Update App.js

--- mvc/App.js
+++ mvc/App.js
@@ -1,23 +1,51 @@
-const App = {
-  init() {
-    this.fetchData();
-    this.bindEvents();
-  },
-  fetchData() {
-    Model.getData().then(data => {
-      View.renderData(data);
-    });
-  },
-  bindEvents() {
-    document.querySelector('#save').addEventListener('click', () => {
-      this.saveData();
-    });
-  },
-  saveData() {
-    Model.saveData().then(() => {
-      this.fetchData();
-    });
-  }
+// Define Model
+const model = {
+    pages: {
+        home: {
+            title: 'Home',
+            content: '<h1>Welcome to my website!</h1>'
+        },
+        about: {
+            title: 'About',
+            content: '<h1>About me</h1><p>I am a web developer.</p>'
+        },
+        contact: {
+            title: 'Contact',
+            content: '<h1>Contact me</h1><p>Email: john@example.com</p>'
+        }
+    }
 };
 
-App.init();
+// Define View
+const view = {
+    renderPage: function(page) {
+        const contentElement = document.getElementById('content');
+        contentElement.innerHTML = page.content;
+        document.title = page.title;
+    }
+};
+
+// Define Controller
+const controller = {
+    navigateToPage: function(pageName) {
+        const page = model.pages[pageName];
+        view.renderPage(page);
+        window.location.hash = pageName;
+    },
+    init: function() {
+        const hash = window.location.hash.substr(1);
+        if (hash) {
+            controller.navigateToPage(hash);
+        } else {
+            controller.navigateToPage('home');
+        }
+
+        window.addEventListener('hashchange', function() {
+            const hash = window.location.hash.substr(1);
+            controller.navigateToPage(hash);
+        });
+    }
+};
+
+// Initialize the application
+controller.init();
